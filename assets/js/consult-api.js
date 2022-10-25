c0835930f2d9ee1ccb9940d8606b8b75f3fcb597
Juan Cruz <juancruzf017@gmail.com>
2022-10-25 18:07:14 -0300

    agrego detalle de producto con conexión a una api

---
+++ assets/js/consult-api.js
@@ -0,0 +1,23 @@
+function getImage() {
+ console.log("function called...");
+
+ let data
+
+
+//  fetch('https://api.unsplash.com/photos/random', {
+//         method: 'GET',
+//         headers: {
+//             "Content-type": "application/json",
+//             "Authorization": "Client-ID WoQ1uG4s2WbJceUvu5yJkkCe3lFwmOQibGjOuJjBGko"
+//     }}).then(response => response.json())
+//     .then(json => {cargarImagen(json)})
+
+}
+
+function cargarImagen(pData){
+    console.info(pData) 
+  
+    document.getElementById('my-image').src=pData.urls.regular;
+}
+
+     
