6629a07c8fff2107df69652ec039fe671760a484
Juan Cruz <juancruzf017@gmail.com>
2022-10-25 19:08:26 -0300

    conexión a la  api

--- assets/js/consult-api.js
+++ assets/js/consult-api.js
@@ -4,13 +4,13 @@ function getImage() {
  let data
 
 
-//  fetch('https://api.unsplash.com/photos/random', {
-//         method: 'GET',
-//         headers: {
-//             "Content-type": "application/json",
-//             "Authorization": "Client-ID WoQ1uG4s2WbJceUvu5yJkkCe3lFwmOQibGjOuJjBGko"
-//     }}).then(response => response.json())
-//     .then(json => {cargarImagen(json)})
+ fetch('https://api.unsplash.com/photos/random', {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": "Client-ID WoQ1uG4s2WbJceUvu5yJkkCe3lFwmOQibGjOuJjBGko"
+    }}).then(response => response.json())
+    .then(json => {cargarImagen(json)})
 
 }
 
